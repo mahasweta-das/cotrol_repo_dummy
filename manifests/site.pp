461673c8772eb8cd94adbe75cf5696a4aed8e6b2
mahasweta-das <40578374+mahasweta-das@users.noreply.github.com>
2019-08-01 15:25:45 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,9 @@
-node 'master.puppet.vm'{
-  include role:master_server
-}
 node default { # shell based 
-  file { '/root/README': #resource name
-    ensure => file, # parameter 
-    content => 'This is a readme',
-    owner   => 'root', #last should also have comma at the end
+#  file { '/root/README': #resource name
+#    ensure => file, # parameter 
+#    content => 'This is a readme',
+#    owner   => 'root', #last should also have comma at the end
   }
+node 'master.puppet.vm' {
+  include role::master_server
 }
